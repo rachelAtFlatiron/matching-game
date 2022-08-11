3e0a065d473397efd11d34d3ead00d4ccaffeb0c
Rachel Katz <rachel.katz@flatironschool.com>
2022-08-11 14:09:04 -0400

    lotsa props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function Card({ word, handleClick }) {
-    
+
   return (
     <div onClick={handleClick} style={{border: "solid 1px black"}}>{ word }</div>
-  )
+  )  
 }
