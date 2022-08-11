9a9f323af237b5aeda3e58c7184a9ae1e39f58f2
Rachel Katz <rachel.katz@flatironschool.com>
2022-08-11 16:07:06 -0400

    comments and todos

--- src/App.js
+++ src/App.js
@@ -24,25 +24,33 @@ function App() {
     }
   ]
 
-  
+
+  /*
+    TODOS:
+    1. create state to keep track of current color and stuff card
+    2. update state on card click
+    3. set card to be either black or red based on state
+    4. create on enter key event evaluate selection
+
+  */
   return (
     <div className="App">
-
+      
       <div style={{display: "flex"}} className="cards-container">
+        {/* Left cards */}
         <div className="card-section colors">
             {
               colorPairs.map((el) => <Card word={el.left} />)
             }
         </div>
 
-        
+        {/* Right cards */}
         <div className='card-section stuff'>
           {
             colorPairs.map((el) => <Card word={el.right} />)
           }
         </div>
       </div>
-      {/* <Form /> */}
     </div>
   );
 }
